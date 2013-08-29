1d7c02afa4ef4d0768320dbed01917c619333f5f
Harrison Wray <hwray@stanford.edu>
2013-08-29 15:26:41 -0700

    NO FADING. THANKS NIELS. I THOUGHT THIS WAS AMERICA.

--- src/public/js/Interactions.js
+++ src/public/js/Interactions.js
@@ -299,11 +299,11 @@ hammertime.on("dragend", function(event) {
 
     clearFeedback(); 
 
-    fadeContainers(0); 
+    // fadeContainers(0); 
 
-    window.setTimeout(function() {
+    // window.setTimeout(function() {
       func(event); 
-    }, 300); 
+    // }, 300); 
   } else {
     colorTreeTex(sharedParent, "#06c4f9"); 
     clearFeedback(); 
